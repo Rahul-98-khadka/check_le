922fc503259f559786336a5b1c5cccac9dddc7c0
Rahul Raj <khadkarhl@gmail.com>
2023-03-06 21:42:49 +0530

    The file is about Simple intrest.

---
+++ simple.py
@@ -0,0 +1,9 @@
+p = float(input("Enter the principal amount: "))
+t = float(input("Enter the time in years: "))
+r = float(input("Enter the rate of interest: "))
+
+# Calculate simple interest
+si = (p * t * r) / 100
+
+# Display the result
+print("The simple interest is:", si)
